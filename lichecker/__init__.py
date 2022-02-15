0c7106d63f1489ec2e4a3103536bff1180994183
jarbasai <jarbasai@mailfence.com>
2022-02-15 21:21:27 +0000

    hpnd

--- lichecker/__init__.py
+++ lichecker/__init__.py
@@ -82,15 +82,15 @@ class DependencyChecker:
 
 class LicenseChecker(DependencyChecker):
     ALIASES = {
-        'ASL 2.0': 'Apache-2.0'
+        'ASL 2.0': 'Apache-2.0',
+        "Historical Permission Notice and Disclaimer": "HPND"
     }
 
     def __init__(self, pkg_name, license_overrides=None, whitelisted_packages=None,
                  allow_nonfree=False, allow_viral=False, allow_unknown=False,
                  allow_unlicense=False, allow_lgpl=False, allow_ambiguous=False, allow_public_domain=True):
         super().__init__(pkg_name)
-        license_overrides = license_overrides or {}
-        self._license_overrides = {k.lower(): v for k, v in license_overrides.items()}
+        self._license_overrides = license_overrides or {}
         self._whitelist = whitelisted_packages or []
         self.allow_nonfree = allow_nonfree
         self.allow_viral = allow_viral
@@ -125,11 +125,11 @@ class LicenseChecker(DependencyChecker):
 
     @property
     def licenses(self):
-        return {p.lower(): self._license_overrides.get(p.lower()) or self.get_package_data(p).get("License")
+        return {p: self._license_overrides.get(p) or self.get_package_data(p).get("License")
                 for p in self.transient_dependencies}
 
     def validate(self):
-        valid = ["mit", 'apache-2.0', 'unlicense', 'mpl-2.0', 'isc', 'bsd', 'psf', 'zpl']
+        valid = ["mit", 'apache-2.0', 'unlicense', 'mpl-2.0', 'isc', 'bsd', 'psf', 'zpl', "hpnd"]
         pkgs = [(self.pkg_name, self.license)] + \
                [(pkg, li) for pkg, li in self.licenses.items()]
         for pkg, li in pkgs:
